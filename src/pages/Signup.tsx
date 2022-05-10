58eb3ddbf583ad9b254483136b9deccb09fccd62
Julyy <66120479+kwakjy@users.noreply.github.com>
2022-05-10 10:31:19 +0900

    Feat: Implement Signup page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,55 @@
+import {Button, Container, Form} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+import {ChangeEvent, useCallback, useState} from "react";
+
 const Signup = () => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [isManager, setIsManager] = useState(true);
+  const [isStaff, setIsStaff] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleIdChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setId(event.target.value)
+  }, [])
+
+  const handlePasswordChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value)
+  }, [])
+
+  const handleCheckboxClick = () => {
+    setIsManager(!isManager)
+    setIsStaff(!isStaff)
+  }
+
+  const handleSignupButtonClick = () => {
+    alert("회원가입에 성공하였습니다.")
+    navigate("/login")
+  }
+
   return (
-    <div>회원가입 페이지입니다.</div>
+      <Container style={{justifyContent: "center", marginTop: 200, marginLeft: 500}}>
+        <h2>회원가입</h2>
+        <Form style={{borderWidth: 10, width: 500}}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>ID</Form.Label>
+            <Form.Control type="id" placeholder="아이디를 입력하세요" value={id} onChange={handleIdChange}/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="비밀번호를 입력하세요" value={password} onChange={handlePasswordChange}/>
+          </Form.Group>
+
+          <div style={{display: "flex", marginBottom: 10}}>
+            <Form.Check style={{marginRight: 10}} label="관리자" checked={isManager} onClick={() => handleCheckboxClick()}/>
+            <Form.Check label="직원" checked={isStaff} onClick={() => handleCheckboxClick()}/>
+          </div>
+
+          <Button style={{width: 120}} onClick={() => handleSignupButtonClick()}>회원가입</Button>
+        </Form>
+      </Container>
   )
 }
 
