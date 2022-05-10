169e40a9341dd4627ce2dec5c1d659d5496608e3
Julyy <66120479+kwakjy@users.noreply.github.com>
2022-05-10 09:15:13 +0900

    Feat: Implement Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,21 @@
+import {Link} from "react-router-dom";
+import {Button, Container} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Home = () => {
+
   return (
-    <div>홈 페이지입니다.</div>
+    <Container style={{marginTop: 300}}>
+      <h1 style={{textAlign: "center"}}>근태 관리 사이트</h1>
+      <div style={{textAlign: "center"}}>
+        <Link to="/login">
+          <Button style={{marginRight: 10, width: 100}}>로그인</Button>
+        </Link>
+        <Link to="/signup">
+          <Button style={{width: 150}}>회원가입</Button>
+        </Link>
+      </div>
+    </Container>
   )
 }
 
