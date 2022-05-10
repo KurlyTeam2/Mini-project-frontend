7f1508d7b44a191940006b605b32474eec32d35e
Julyy <66120479+kwakjy@users.noreply.github.com>
2022-05-10 10:57:33 +0900

    Design: Add Staff page layout

--- src/pages/Staff.tsx
+++ src/pages/Staff.tsx
@@ -1,6 +1,20 @@
+import {Container} from "react-bootstrap";
+
 const Staff = () => {
   return (
-    <div>직원 페이지입니다.</div>
+    <Container className="border border-dark">
+      <Container style={{width: 1235, height: 100, marginTop: 20, marginBottom: 20}} className="border border-dark">
+        <h2>헤더 (내정보)</h2>
+      </Container>
+      <Container style={{display: "flex"}}>
+        <Container style={{width: 200, height: 600, marginBottom: 20}} className="border border-dark">
+          <h2>메뉴</h2>
+        </Container>
+        <Container style={{width: 1000, height: 600}} className="border border-dark">
+          <h2>직원 페이지</h2>
+        </Container>
+      </Container>
+    </Container>
   )
 }
 
