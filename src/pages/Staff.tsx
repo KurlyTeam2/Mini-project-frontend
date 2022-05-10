a472ff1195ab45df158ace3528e5081ab8988a1f
GYUMINSHIN <92241822+GYUMINSHIN@users.noreply.github.com>
2022-05-10 17:30:15 +0900

    Add Staff.tsx

--- src/pages/Staff.tsx
+++ src/pages/Staff.tsx
@@ -1,19 +1,116 @@
-import {Container} from "react-bootstrap";
+import {Button} from "react-bootstrap";
+import { ChangeEvent, useCallback, useState } from "react";
+import styled from "styled-components";
+
+class User{
+  name: string;
+  auth: string;
+  day = 0;
+  week = 0;
+  month = 0;
+
+  constructor(name: string, auth: string, day:number, week:number, month:number) {
+    this.name = name;
+    this.auth = auth;
+    this.day = day;
+    this.week = week;
+    this.month = month;
+  }
+}
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: lightblue;
+  width: 100%;
+  padding: 20px 30px;
+  border-bottom: 1px solid;
+`;
+
+const Layout = styled.div`
+  display: flex;
+  width: 60%;
+  height: 100%;
+`;
+
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  padding: 30px 40px;
+`;
+
+const HourBox = styled.div`
+  display: flex;
+  margin-top: 30px;
+  width: 50vw;
+  justify-content: center;
+`;
+
+const BtnBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0px 50px;
+`;
+
+const Text1 = styled.div`
+  font-size: 30px;
+  margin-right: 40px;
+  margin-bottom: 30px;
+`;
+
+const Calendar = styled.div`
+  width: 50vw;
+  height: 20%;
+  margin-bottom: 50px;
+  font-size: 30px;
+  border: 2px solid;
+  text-align: center;
+`;
+
+const staff = new User("컬리2", "Staff", 6, 10, 40);
 
 const Staff = () => {
   return (
-    <Container className="border border-dark">
-      <Container style={{width: 1235, height: 100, marginTop: 20, marginBottom: 20}} className="border border-dark">
-        <h2>헤더 (내정보)</h2>
-      </Container>
-      <Container style={{display: "flex"}}>
-        <Container style={{width: 200, height: 600, marginBottom: 20}} className="border border-dark">
-          <h2>메뉴</h2>
-        </Container>
-        <Container style={{width: 1000, height: 600}} className="border border-dark">
-          <h2>직원 페이지</h2>
-        </Container>
-      </Container>
+    <Container>
+      <Header>
+        <h1 style={{marginRight:20, marginLeft: 50}}>{staff.name}</h1>
+        <h4>({staff.auth})</h4>
+      </Header>
+      <Layout>
+        <Content>
+          <Calendar>일주일 달력
+          </Calendar>
+          <h4 style={{ marginLeft: 5 }}>5/10(화)</h4>
+          <HourBox>
+            <BtnBox>
+              <Text1>출근 시간 10:00</Text1>
+              <Button>출근</Button>
+            </BtnBox>
+            <BtnBox>
+              <Text1>퇴근 시간 10:00</Text1>
+              <Button>퇴근</Button>
+            </BtnBox>
+          </HourBox>
+          <h1 style={{marginTop:50}}>출근 통계</h1>
+          
+          <HourBox style={{justifyContent:"flex-start"}}>
+            <Text1>일: {staff.day}시간</Text1>
+            <Text1>주: {staff.week}시간</Text1>
+            <Text1>월: {staff.month}시간</Text1>
+          </HourBox>
+        </Content>
+      </Layout>
     </Container>
   )
 }
